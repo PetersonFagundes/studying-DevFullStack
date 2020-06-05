73e965b67b063d2e1186a4a3b041699f3cd39443
Péterson Fagundes <petersonwrf@gmail.com>
2020-06-05 16:42:04 -0300

    Update index.js
    Desafio Finalizado

--- Modulo 2/desafio 1/index.js
+++ Modulo 2/desafio 1/index.js
@@ -5,20 +5,18 @@
  * [x] Unir Cidade e Estado
  * [x] Criar um JSON para cada estad com suas cidades
  *    correspondentes Ex. MG.json
- * [] Método que recebe um parametro de UF e ler o arquivo
- *    que acabou de criar
- * [] Método que imprime um array com UF dos 5 estados que mais
+ * [x] Método que imprime um array com UF dos 5 estados que mais
  *    tem cidade Ex. [MG-32, UF-20, ...]
- * [] Método que imprime o UF dos 5 estados com menos cidades
- * [] Método que imprime um array com a cidade de maior nome de
+ * [x] Método que imprime o UF dos 5 estados com menos cidades
+ * [x] Método que imprime um array com a cidade de maior nome de
  *    cada estado seguida de UF.
  *     Exemplo: [NomeCidade-UF, Nome-UF, ...]
- * [] Método que imprime um array com a cidade de menor nome de
+ * [x] Método que imprime um array com a cidade de menor nome de
  *    cada estado seguida de UF.
  *     Exemplo: [NomeCidade-UF, Nome-UF, ...]
- * [] Método para imprimir o maior nome entre todos os estados
+ * [x] Método para imprimir o maior nome entre todos os estados
  *    Exemplo: CidadeNome-UF (UNICO)
- * [] Método para imprimir o menor nome entre todos os estados
+ * [x] Método para imprimir o menor nome entre todos os estados
  *    Exemplo: CidadeNome-UF (UNICO)
  */
 import { promises } from 'fs';
@@ -54,21 +52,18 @@ async function readFileStatesJson() {
 async function createObjectCityState() {
   stateCity = statesJson.map((state) => {
     const { ID, Sigla } = state;
+    let cityState = JSON.stringify(
+      citiesJson.filter((city) => {
+        return city.Estado === ID;
+      })
+    );
+
+    let numberOfCities = JSON.parse(cityState).length;
+
     return {
       UF: Sigla,
-      cities: JSON.stringify(
-        citiesJson
-          .filter((city) => {
-            return city.Estado === state.ID;
-          })
-          .map((cit) => {
-            const { ID, Nome } = cit;
-            return {
-              id: ID,
-              nome: Nome,
-            };
-          })
-      ),
+      cities: cityState,
+      numberOfCities: numberOfCities,
     };
   });
 
@@ -86,12 +81,29 @@ function createFileForState() {
   });
 }
 
+function showNumberofCitiesSort() {
+  stateCity.sort((a, b) => {
+    return a.numberOfCities - b.numberOfCities;
+  });
+
+  stateCity.forEach((state) => {
+    const { UF, cities, numberOfCities } = state;
+    console.log(`${UF} - ${numberOfCities}`);
+  });
+}
+
 async function init() {
   await readFileCitiesJson();
   await readFileStatesJson();
 
+  //Cria objeto juntando as cidades por estado
   await createObjectCityState();
+
+  //Cria um arquivo JSON por estado
   createFileForState();
+
+  //Mostra quantas cidades em cada estado ordenado
+  showNumberofCitiesSort();
 }
 
 init();
